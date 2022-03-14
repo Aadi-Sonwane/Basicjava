5fb7f0368a50b5e1a6e6ab9b78c18f1e83d22862
Aadi-Sonwane <aadus.info@gmail.com>
2022-03-14 14:40:02 +0530

    starting my project

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+//package com.company;
+//
+//public class Main {
+//    int id;
+//    String name;
+//
+//
+//    public static void main(String[] args) {
+//	// write your code here
+//        System.out.print("hello man!");
+//        String name = "aadi";
+//        System.out.println(name);
+//        Main s1 = new Main();
+//        System.out.println(s1.id);
+//        System.out.println(s1.name);
+//    }
+//}
+
+package com.company;
+
+public class Main {
+    int id;
+    String name;
+}
+
+    class newone{
+    public static void main(String[] args) {
+        Main s1 = new Main();
+        s1.id = 303;
+        s1.name = "new one added bro ";
+        System.out.println(s1.id +"\n"+s1.name);
+        System.out.println(s1.name);
+    }
+}
